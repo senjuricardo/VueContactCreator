245bf6e57355501bedb214cae2c323db89aac9d4
Ricardo <ricardo.sousa13@hotmail.com>
2022-10-21 19:29:46 +0100

    teste vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 const routes= [
     {path: '/',name: "Main", component: ()=> import('../views/Listagem.vue')},
     {path: '/create',name: "Create", component: ()=> import('../views/CreateTodo.vue')},
-    // {path: '/list',name: "FormList", component: ()=> import('../components/Users.vue')},
+    
      {path: '/detail/:id',name: "Detail", component: ()=> import('../views/Detail.vue')}
 ]
 
